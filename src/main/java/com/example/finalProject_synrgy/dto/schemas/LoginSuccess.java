a2387b1a3015f58dfad06c33b79cbd6e2842de44
doddy-s <doddy.s@students.amikom.ac.id>
2024-07-26 19:01:19 +0700

    docs: create responses documentation for auth controller

---
+++ src/main/java/com/example/finalProject_synrgy/dto/schemas/LoginSuccess.java
@@ -0,0 +1,5 @@
+package com.example.finalProject_synrgy.dto.schemas;
+
+public class LoginSuccess extends BaseSchema {
+    public LoginSchema data;
+}
---
+++ src/main/java/com/example/finalProject_synrgy/dto/schemas/StringDataSchema.java
@@ -0,0 +1,5 @@
+package com.example.finalProject_synrgy.dto.schemas;
+
+public class StringDataSchema extends BaseSchema {
+    public String data;
+}
